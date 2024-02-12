36496af6d8b4a9800b449dce4a4b434c7ae38be7
ho3in <cash56isii89nm@gmail.com>
2024-02-12 10:10:05 +0330

    add post.py

---
+++ test.py
@@ -0,0 +1,21 @@
+from pytube import YouTube, Playlist
+import os
+os.system('clear')
+
+
+def resolution(url):
+    yt = YouTube(url)
+    video = yt.streams
+    for i in video:
+        print(i)
+
+
+def download(url:str, res:str):
+    yt = YouTube(url)
+    video = yt.streams.get_by_resolution(res)
+    video.download()
+
+
+s = input('linki at galsin: ')
+
+resolution(s)
